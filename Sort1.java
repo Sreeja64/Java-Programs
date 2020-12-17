50f12bbdaf6763ea4fe1897fd5bb089bff9d6b7e
Sreeja De <sreejade.sd@gmail.com>
2020-12-18 00:42:54 +0530

    Done

---
+++ Sort1.java
@@ -0,0 +1,26 @@
+class Sort1
+{
+	public static void main(String args[])
+	{	
+		int a[]={3,1,4,2},key,j;
+		for(int k=0;k<4;k++)
+				System.out.print(a[k]+" ");
+		System.out.println();
+		for(int i=1;i<4;i++)
+		{
+			key=a[i];j=i-1;
+			while(j>=0 && key<=a[j])
+			{
+				a[j+1]=a[j];
+				j--;
+			}
+			a[j+1]=key;
+			for(int k=0;k<4;k++)
+				System.out.print(a[k]+" ");
+			System.out.println();
+		}
+		System.out.println("The sorted array is ");
+		for(int i=0;i<4;i++)
+			System.out.print(a[i]+" ");
+	}
+}
